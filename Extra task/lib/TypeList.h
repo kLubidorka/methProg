05231697e366b34caeb4f2ed8145c5ca9e2817f8
Ivan <39248996+kLubidorka@users.noreply.github.com>
2020-12-17 14:15:19 +0400

    upd typelist

--- Extra task/lib/TypeList.h
+++ Extra task/lib/TypeList.h
@@ -1,28 +1,29 @@
-#pragma once
+#ifndef SYMMETRICDISPATCHER_TYPELIST_H
+#define SYMMETRICDISPATCHER_TYPELIST_H
 
+class Nulltype {
+};
 
-class Nulltype {};
-
-template <typename ...Args>
-struct TypeList
-{
+template<typename ...Args>
+struct TypeList {
     using Head = Nulltype;
     using Tail = Nulltype;
 };
 
 typedef TypeList<> EmptyTypeList;
 
-template <typename H, typename ...T>
-struct TypeList<H, T...>
-{
+template<typename H, typename ...T>
+struct TypeList<H, T...> {
     using Head = H;
     using Tail = TypeList<T...>;
 };
 
 
-template <typename H>
-struct TypeList<H>
-{
+template<typename H>
+struct TypeList<H> {
     using Head = H;
     using Tail = EmptyTypeList;
 };
+
+
+#endif //SYMMETRICDISPATCHER_TYPELIST_H
