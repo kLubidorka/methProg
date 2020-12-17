2e8520bff5330741b8f258237c94c8b7184163b7
Ivan <39248996+kLubidorka@users.noreply.github.com>
2020-12-17 17:15:47 +0400

    upd proj structure

---
+++ Extra task/MeetingExecutor.h
@@ -0,0 +1,49 @@
+#ifndef SYMMETRICDISPATCHER_MEETINGEXECUTOR_H
+#define SYMMETRICDISPATCHER_MEETINGEXECUTOR_H
+
+#include "TestClasses.h"
+
+class Meeting {
+public:
+    static void meet(Dog *one, Dog *two) {
+        std::cout << "Bark Bark" << std::endl;
+    }
+
+    static void meet(Dog *one, Cat *two) {
+        std::cout << "Bark Meow" << std::endl;
+    }
+
+    static void meet(Cat *one, Dog *two) {
+        std::cout << "Bark Meow" << std::endl;
+    }
+
+    static void meet(Cat *one, Cat *two) {
+        std::cout << "Meow Meow" << std::endl;
+    }
+
+    static void meet(Dog *one, CatDog *two) {
+        std::cout << "Who are you?" << std::endl;
+    }
+
+    static void meet(CatDog *one, Dog *two) {
+        std::cout << "Who are you?" << std::endl;
+    }
+
+    static void meet(Cat *one, CatDog *two) {
+        std::cout << "Who are you?" << std::endl;
+    }
+
+    static void meet(CatDog *one, Cat *two) {
+        std::cout << "Who are you?" << std::endl;
+    }
+
+    static void meet(CatDog *one, CatDog *two) {
+        std::cout << "Who are you?" << std::endl;
+    }
+
+    static void meet(...) {
+        std::cout << "Something bad happend" << std::endl;
+    }
+};
+
+#endif //SYMMETRICDISPATCHER_MEETINGEXECUTOR_H
--- Extra task/Tests.cpp
+++ Extra task/Tests.cpp
@@ -2,32 +2,29 @@
 #include "BasicDispatcher.h"
 #include "StaticDispatcher.h"
 #include "TestClasses.h"
+#include "MeetingExecutor.h"
 
 int main()
 {
     Animal* cat1 = new Cat;
     Animal* dog1 = new Dog;
-    Animal* dog2 = new Dog;
-    Animal* cat2 = new Cat;
     Animal* catdog1 = new CatDog;
 
-    std::cout << std::endl << typeid(*cat1).name() << " " << typeid(Cat).name() << std::endl;
-    std::cout << std::endl;
-
     StaticDispatcher<Meeting, Animal, TypeList< Dog, Cat, CatDog > >::Go(catdog1, cat1);
+    StaticDispatcher<Meeting, Animal, TypeList< Dog, Cat, CatDog > >::Go(dog1, cat1);
 
-    BasicDispatcher<Animal, Animal> bdAnim;
-    bdAnim.add<Dog, Dog>(MeetDog);
-    bdAnim.add<Cat, Dog>(MeetCatDog);
-    //bdAnim.add<Dog, Cat>(MeetCatDog);
-    bdAnim.add<Cat, Cat>(MeetCat);
-
-    bdAnim.Go(cat1, dog1);
-    bdAnim.Go(cat1, cat2);
-
-    // Переписать BasicDispatcher на unordered_map
-    // Реализовать симметричный StaticDispatcher
-
-    // TypeList <dog, cat, catdog>
-    // meet (d,d), (d,c), (d,cd), (c,c), (c,cd), (cd,cd)
+//    BasicDispatcher<Animal, Animal> bdAnim;
+//    bdAnim.add<Dog, Dog>(MeetDog);
+//    bdAnim.add<Cat, Dog>(MeetCatDog);
+//    //bdAnim.add<Dog, Cat>(MeetCatDog);
+//    bdAnim.add<Cat, Cat>(MeetCat);
+//
+//    bdAnim.Go(cat1, dog1);
+//    bdAnim.Go(cat1, cat2);
+//
+//    // Переписать BasicDispatcher на unordered_map
+//    // Реализовать симметричный StaticDispatcher
+//
+//    // TypeList <dog, cat, catdog>
+//    // meet (d,d), (d,c), (d,cd), (c,c), (c,cd), (cd,cd)
 }
